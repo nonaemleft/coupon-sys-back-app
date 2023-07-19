3dab46f0ac64f92a2863018141b67878f36e0b2b
חן <napoleon14@gmail.com>
2023-07-19 12:51:22 +0300

    Baby Steps...

---
+++ src/main/java/com/chen/couponys/CouponysApplication.java
@@ -0,0 +1,13 @@
+package com.chen.couponys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponysApplication.class, args);
+	}
+
+}
